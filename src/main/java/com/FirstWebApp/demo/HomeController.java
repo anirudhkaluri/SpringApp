5796a35e270dd022e9d125d622175dc2e7b252ed
anirudhkaluri <anirudhkaluri@Anirudh-Kaluris-Mac.local>
2023-02-15 14:17:18 +0530

    we used parameter name which we are receiving exactly in the controller

--- src/main/java/com/FirstWebApp/demo/HomeController.java
+++ src/main/java/com/FirstWebApp/demo/HomeController.java
@@ -9,36 +9,55 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
-	
+	/*
 	@RequestMapping("home")
 	// @ResponseBody //this says that whatever the function is returning is data
 	//but what we want is to return a page
-	public String home(HttpServletRequest req) {
-		System.out.println("\n\n\n\n\n\n\nwe are in the home controller\n\n\n\n\n\n\n\n");
-
-		String name=req.getParameter("name");
-		System.out.println("Hi the name is "+name);
-		//now we got the parameter name with us
-		//how to send it to home.jsp so that we could display the name parameter?
-		//we can use httpSession
-		HttpSession session=req.getSession();
-		String sessID=session.getId();
-		System.out.println("The session ID is "+sessID);
-		session.setAttribute("name",name);
-		//now lets put it on home.jsp page
-		//we inject this into home.jsp by using ${name} in home.jsp just like how we did in Node in EJS engine
-		return "home";
-		//it will return home.jsp
-		//how will dispatcher know where to search for home.jsp?
-		//Its autoconfigured that pages are in main/ webapp/
-		//jsp is a special type. //When springboot came into picture REST APIs and microservices are more famous
-		//The focus is on REST APIs
-		//By default spring wont send JSPs. So we add a tomcat-jasper dependency in pom.xml
-		//we get that dependency by searching it in mvn repo online
-		//Spring will send Response
-		// Now let us remove .jsp extension. 
-		// also lets change the location of home.jsp from /src/main/webapp/ to /src/main/webapp/pages
+	
+	 
+	 			public String home(HttpServletRequest req) {
+				System.out.println("\n\n\n\n\n\n\nwe are in the home controller\n\n\n\n\n\n\n\n");
 		
+				String name=req.getParameter("name");
+				System.out.println("Hi the name is "+name);
+				//now we got the parameter name with us
+				//how to send it to home.jsp so that we could display the name parameter?
+				//we can use httpSession
+				HttpSession session=req.getSession();
+				String sessID=session.getId();
+				System.out.println("The session ID is "+sessID);
+				session.setAttribute("name",name);
+				//now lets put it on home.jsp page
+				//we inject this into home.jsp by using ${name} in home.jsp just like how we did in Node in EJS engine
+				return "home";
+				//it will return home.jsp
+				//how will dispatcher know where to search for home.jsp?
+				//Its autoconfigured that pages are in main/ webapp/
+				//jsp is a special type. //When springboot came into picture REST APIs and microservices are more famous
+				//The focus is on REST APIs
+				//By default spring wont send JSPs. So we add a tomcat-jasper dependency in pom.xml
+				//we get that dependency by searching it in mvn repo online
+				//Spring will send Response
+				// Now let us remove .jsp extension. 
+				// also lets change the location of home.jsp from /src/main/webapp/ to /src/main/webapp/pages
+		
+				}
+	 
+	 TILL HERE WE USED CONCEPTS WHICH ARE THERE IN SERVLETS WE DIDNT USE CONCEPTS WHICH LEVERAGE THE POWER OF SPRING
+	 
+	 
+	 */
+	 
+	@RequestMapping("home")
+	public String home(String name,HttpSession session) {
+		//this is more easier
+		//we get the data with key named "name", that will directly be received by the controller
+		//also we dont have to create HttpSession object 
+		//using dependency injection Spring will directly look for/create a session object associated with the user
+		
+		session.setAttribute("name", name);
+		return "home";
 	}
+	
 
 }
